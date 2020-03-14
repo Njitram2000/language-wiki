2dab8bd094008391af810aaba10427a5d7b25a13
Martijn <mvanloocke@gmail.com>
2020-03-14 17:32:08 +0100

    Toggle hangul keyboard for any text field and have keyboard input switch between the 2 character sets

--- wiki.js
+++ wiki.js
@@ -1,23 +1,43 @@
 'use strict';
 
 class Wiki extends React.Component {
+  initialWikiState = {
+    loading: true, 
+    filteredData: [],
+    showVirtualKeyboard: false,
+    focussedInput: null
+  };
+
   constructor(props) {
     super(props);
-    this.state = { loading: true, data: [] };
-    const that = this;
+    this.state = this.initialWikiState;
     $.getJSON({url: '/wiki', success: (data) => {
-      this.setState((state, props) => ({loading: false, data}));
+      this.data = data;
+      this.setState((state, props) => ({loading: false, filteredData: data}));
     }});
   }
 
+  data = [];
+
   onKeyUp = (event) => {
-    // console.log(event.target.value);
+    //
+  }
+  onFocus = (event) => {
+    var focussedInput = event.target.getAttribute('data-name');
+    this.setState((state, props) => ({focussedInput}), () => {
+      if (this.state.showVirtualKeyboard) {
+        resetHangulBindings();
+        hangulKeyboard();
+      }
+    });
   }
 
-  componentDidMount() {
-    setTimeout(() => {
-      hangulKeyboard();
-    }, 100);
+  toggleVirtualKeyboard = (event) => {
+    this.setState((state, props) => ({showVirtualKeyboard: !state.showVirtualKeyboard}), () => {
+      if (!this.state.showVirtualKeyboard) {
+        resetHangulBindings();
+      }
+    })
   }
 
   render() {
@@ -25,22 +45,36 @@ class Wiki extends React.Component {
       return 'loading...';
     } else {
       return  <div>
-                <div><input id="hangul-editor" type="text" onKeyUp={this.onKeyUp}/></div>
-                {this.state.data.map((entry) => {
-                  return  <div key="entry.title">
-                            <div>{entry.title}</div>
-                            <div>{entry.tags}</div>
-                            <div>{entry.source}</div>
-                            <div>{entry.details}</div>
-                            <div className="examples">
-                              {entry.examples.map((example) => Object.keys(example).map((lang =>
-                                <div key={lang}>{example[lang]}</div>
-                              )))}
-                            </div>
-                            
-                          </div>;
-                })}
-                <div id="keyboard"></div>
+                <div className='search'>
+                  <input type='text'
+                         data-name='search'
+                         id={this.state.showVirtualKeyboard && this.state.focussedInput === 'search' ? 'hangul-input': undefined}
+                         onFocus={this.onFocus} onBlur={this.onBlur} onKeyUp={this.onKeyUp} />
+                  <img className='korea' src="korea.png" onClick={this.toggleVirtualKeyboard} />
+                </div>
+                <div id='previews'>
+                  {this.state.filteredData.map((entry, index) => {
+                    return  <div key={index}>
+                              <div className='title'>{entry.title}</div>
+                              <div className='tags'>{entry.tags.map((tag) => `#${tag} `)}</div>
+                              <div>{entry.source}</div>
+                              <div>{entry.details}</div>
+                              <div className='examples'>
+                                {entry.examples.map((example) => Object.keys(example).map((lang =>
+                                  <div key={lang}>{example[lang]}</div>
+                                )))}
+                              </div>
+                            </div>;
+                    })
+                  }
+                </div>
+                <div id='view'>
+                  
+                </div>
+                <div id='editor'>
+
+                </div>
+                <div id='hangul-keyboard' className={this.state.showVirtualKeyboard && this.state.focussedInput !== null ? '' : 'hidden'}></div>
               </div>;
     }
   }
